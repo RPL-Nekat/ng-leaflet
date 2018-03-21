c64b6ca56f4f0a890af55c706e9ff83bd1fedd96
Falih <dudu.kotkot@gmail.com>
2018-03-21 10:34:14 +0700

    fungsi menambah lokasi tanpa coord

---
+++ src/app/form-add/form-add.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, EventEmitter, Input } from '@angular/core';
+import { MapService } from '../services/map.service';
+import { Location } from '../model/location';
+@Component({
+  selector: 'app-form-add',
+  templateUrl: './form-add.component.html',
+  styleUrls: ['./form-add.component.css']
+})
+export class FormAddComponent implements OnInit {
+
+  
+  public locationName: string;
+  public locationDesc: string;
+
+  constructor(private mapService: MapService) {
+    this.locationName = '';
+    this.locationDesc = '';
+   }
+
+  ngOnInit() {
+  }
+
+  private addLocation():void{
+    this.mapService.addLocation(this.locationName, this.locationDesc);
+    this.locationName = '';
+    this.locationDesc= '';
+    alert("success to add!");
+  }
+}
