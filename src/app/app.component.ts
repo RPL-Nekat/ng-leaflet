c6ab5c81401f2463d7fc100909bbf89674bcce3e
Falih <dudu.kotkot@gmail.com>
2018-03-21 10:30:37 +0700

    berisi map.component

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MapService } from './services/map.service';
+import {Location} from './model/location';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
