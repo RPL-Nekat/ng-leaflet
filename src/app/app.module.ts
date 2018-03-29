31ef1a10b4220e9215116fde4eee8c8592daee08
Falih <dudu.kotkot@gmail.com>
2018-03-29 11:32:06 +0700

    import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { FormsModule } from '@angular/forms';
-
+import 'leaflet';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { FormAddComponent } from './form-add/form-add.component';
 import { ListViewComponent } from './list-view/list-view.component';
 import { MapService } from './services/map.service';
 import { ListItemComponent } from './list-item/list-item.component';
+import { HttpModule, Headers } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -17,11 +18,12 @@ import { ListItemComponent } from './list-item/list-item.component';
     MapComponent,
     FormAddComponent,
     ListViewComponent,
-    ListItemComponent
+    ListItemComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [MapService],
   bootstrap: [AppComponent]
